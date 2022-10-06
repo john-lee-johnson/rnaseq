5b65b5f309e2926c4a615a7f92e7ce39d91582ce
John Johnson <johnlee.johnson@gmail.com>
2022-10-06 16:32:08 -0400

    Write gene and transcript lengths

--- bin/salmon_tximport.r
+++ bin/salmon_tximport.r
@@ -72,6 +72,7 @@ build_table = function(se.obj, slot) {
 if(exists("gse")){
     write.table(build_table(gse, "abundance"), paste(c(prefix, "gene_tpm.tsv"), collapse="."), sep="\t", quote=FALSE, row.names = FALSE)
     write.table(build_table(gse, "counts"), paste(c(prefix, "gene_counts.tsv"), collapse="."), sep="\t", quote=FALSE, row.names = FALSE)
+    write.table(build_table(gse, "length"), paste(c(prefix, "gene_lengths.tsv"), collapse="."), sep="\t", quote=FALSE, row.names = FALSE)
     write.table(build_table(gse.ls, "abundance"), paste(c(prefix, "gene_tpm_length_scaled.tsv"), collapse="."), sep="\t", quote=FALSE, row.names = FALSE)
     write.table(build_table(gse.ls, "counts"), paste(c(prefix, "gene_counts_length_scaled.tsv"), collapse="."), sep="\t", quote=FALSE, row.names = FALSE)
     write.table(build_table(gse.s, "abundance"), paste(c(prefix, "gene_tpm_scaled.tsv"), collapse="."), sep="\t", quote=FALSE, row.names = FALSE)
@@ -80,6 +81,7 @@ if(exists("gse")){
 
 write.table(build_table(se,"abundance"), paste(c(prefix, "transcript_tpm.tsv"), collapse="."), sep="\t", quote=FALSE, row.names = FALSE)
 write.table(build_table(se, "counts"), paste(c(prefix, "transcript_counts.tsv"), collapse="."), sep="\t", quote=FALSE, row.names = FALSE)
+write.table(build_table(se, "length"), paste(c(prefix, "transcript_lengths.tsv"), collapse="."), sep="\t", quote=FALSE, row.names = FALSE)
 
 # Print sessioninfo to standard out
 citation("tximeta")
